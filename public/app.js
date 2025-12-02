ea8b234dc29c2f473da9b014d124eefd35d3daa8
Ashraf Wajdi <ashrfwjdii@gmail.com>
2025-12-02 14:52:08 +0800

    removed public folder and server.js

--- public/app.js
+++
@@ -1,6 +0,0 @@
-export async function fetchSheetData() {
-    const res = await fetch('http://localhost:3000/sheetdata');
-    if (!res.ok) throw new Error('Failed to fetch sheet data');
-    const data = await res.json();
-    return data.values; 
-}
